1181e0e955a72b8a4eace3134539d863c2da765f
Stephen Chu <stephen.chu530@gmail.com>
2018-11-08 13:15:06 -0800

    fix: created instance attributes

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,9 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Constructor"""
+        self.id = type(self).id
+        self.created_at = type(self).created_at
+        self.updated_at = type(self).updated_at
         if kwargs:
             for k, v in kwargs.items():
                 self.__setattr__(k, v)
@@ -33,7 +36,6 @@ class BaseModel:
     def to_dict(self):
         """Convert object to dictionary representation"""
         dct = self.__dict__
-        dct['id'] = self.id
         dct['__class__'] = self.__class__.__name__
         dct['created_at'] = self.created_at.isoformat()
         dct['updated_at'] = self.updated_at.isoformat()
