f24253afc6544b6a95b6befafdc2504a18a7273e
Stephen Chu <stephen.chu530@gmail.com>
2018-11-08 15:53:05 -0800

    feat: initial commit

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from base_module import BaseModule
+
+'''Review Class Module'''
+
+
+class Review(BaseModule):
+    '''Review Class Definition'''
+
+    place_id = ''
+    user_id = ''
+    text = ''
