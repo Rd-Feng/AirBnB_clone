f3d9e40e51a98a64e8ce532a1bdb2a99b55c7d7c
Rui Feng <394@holbertonschool.com>
2018-11-08 21:12:55 +0000

    init

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import cmd
+"""entry point for hbnb console"""
+
+
+class HBNBCommand(cmd.Cmd):
+    """ hbnb shell """
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """EOF to exit the program
+        """
+        return True
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
