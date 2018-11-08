935c21d7f8d97c1f53ea67365c11b95ab7064174
Stephen Chu <stephen.chu530@gmail.com>
2018-11-08 14:55:45 -0800

    feat: initial commit

--- models/user.py
+++ models/user.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-"""User Object Class"""
+
+from base_model import BaseModel
+
+'''User Class Module'''
+
+
+class User(BaseModel):
+    '''User Class Definition'''
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
