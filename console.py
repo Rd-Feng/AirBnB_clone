60ef0fe561f59528a0e58434bd0a6981bf6f91cb
Rui Feng <394@holbertonschool.com>
2018-11-09 16:57:52 +0000

    add create and show

--- console.py
+++ console.py
@@ -1,12 +1,48 @@
 #!/usr/bin/python3
 import cmd
 import models
+from models.base_model import BaseModel
 """entry point for hbnb console"""
 
 
 class HBNBCommand(cmd.Cmd):
     """ hbnb shell """
     prompt = '(hbnb) '
+    clslist = {'BaseModel': BaseModel}
+
+    def do_create(self, clsname=None):
+        """Creates a new instance of BaseModel, saves it and prints the id
+        """
+        if not clsname:
+            print('** class name missing **')
+        elif not self.clslist.get(clsname):
+            print('** class doesn\'t exist **')
+        else:
+            obj = self.clslist[clsname]()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, arg):
+        """Show instance based on id"""
+        clsname, objid = None, None
+        args = arg.split(' ')
+        if len(args) > 0:
+            clsname = args[0]
+        if len(args) > 1:
+            objid = args[1]
+        if not clsname:
+            print('** class name missing **')
+        elif not objid:
+            print('** instance id missing **')
+        elif not self.clslist.get(clsname):
+            print("** class doesn't exist **")
+        else:
+            k = clsname + "." + objid;
+            obj = models.storage.all().get(k)
+            if not obj:
+                print('** no instance found **')
+            else:
+                print(obj)
 
     def do_quit(self, arg):
         """Quit command to exit the program
