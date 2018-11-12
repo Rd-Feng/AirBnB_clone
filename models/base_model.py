c0a107baf09e7497f5886b3b11fd59d91d643ddd
Rui Feng <394@holbertonschool.com>
2018-11-12 18:40:00 +0000

    change updated_at for instance

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,7 @@ class BaseModel:
 
     def save(self):
         """Updates the updated_at public instance attribute"""
-        updated_at = datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
