13255e31bef7e767b770484d06f86a22f64f7fc6
Rui Feng <394@holbertonschool.com>
2018-11-12 23:12:45 +0000

    add all builtin commands

--- console.py
+++ console.py
@@ -23,8 +23,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, clsname=None):
-        """Creates a new instance of BaseModel, saves it and prints the id
-        """
+        """Creates a new instance of BaseModel, saves it and prints the id"""
         if not clsname:
             print('** class name missing **')
         elif not self.clslist.get(clsname):
@@ -57,7 +56,8 @@ class HBNBCommand(cmd.Cmd):
                 print(obj)
 
     def do_destroy(self, arg):
-        """destroy instance based on id"""
+        """destroy instance based on id
+        """
         clsname, objid = None, None
         args = arg.split(' ')
         if len(args) > 0:
@@ -80,7 +80,8 @@ class HBNBCommand(cmd.Cmd):
                 models.storage.save()
 
     def do_all(self, arg):
-        """Prints all instances based or not on the class name"""
+        """Prints all instances based or not on the class name
+        """
         if not arg:
             print([str(v) for k, v in models.storage.all().items()])
         else:
@@ -91,11 +92,13 @@ class HBNBCommand(cmd.Cmd):
                    if type(v) is self.clslist.get(arg)])
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and id"""
+        """Updates an instance based on the class name and id
+        """
         from datetime import datetime
+        from shlex import shlex
         clsname, objid, attrname, attrval = None, None, None, None
         updatetime = datetime.now()
-        args = arg.split(' ')
+        args = arg.split(' ', 3)
         if len(args) > 0:
             clsname = args[0]
         if len(args) > 1:
@@ -103,7 +106,7 @@ class HBNBCommand(cmd.Cmd):
         if len(args) > 2:
             attrname = args[2]
         if len(args) > 3:
-            attrval = args[3]
+            attrval = list(shlex(args[3]))[0].strip('"')
         if not clsname:
             print('** class name missing **')
         elif not objid:
@@ -132,8 +135,52 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """EOF to exit the program
         """
-        print()
         return True
+
+    def default(self, line):
+        """handle class commands"""
+        l = line.split('.', 1)
+        if len(l) < 2:
+            print('*** Unknown syntax:', l[0])
+            return False
+        clsname, line = l[0], l[1]
+        if l[0] not in list(self.clslist.keys()):
+            print('*** Unknown syntax: {}.{}'.format(clsname, line))
+            return False
+        l = line.split('(', 1)
+        if len(l) < 2:
+            print('*** Unknown syntax: {}.{}'.format(clsname, l[0]))
+            return False
+        mthname, args = l[0], l[1].rstrip(')')
+        if mthname not in ['all', 'count', 'show', 'destroy', 'update']:
+            print('*** Unknown syntax: {}.{}'.format(clsname, line))
+            return False
+        if mthname == 'all':
+            self.do_all(clsname)
+        elif mthname == 'count':
+            print(self.count_class(clsname))
+        elif mthname == 'show':
+            self.do_show(clsname + " " + args.strip('"'))
+        elif mthname == 'destroy':
+            self.do_destroy(clsname + " " + args.strip('"'))
+        elif mthname == 'update':
+            return False
+            from shlex import shlex
+            l = list(shlex(args, ','))
+            print(l)
+
+    def postloop(self):
+        """print new line after each loop"""
+        print()
+
+    @staticmethod
+    def count_class(clsname):
+        """count number of objects of type clsname"""
+        c = 0
+        for k, v in models.storage.all().items():
+            if type(v).__name__ == clsname:
+                c += 1
+        return (c)
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
