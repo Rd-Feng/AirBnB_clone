3b8958bc7d6500c8d54eda4643f8e44d81503dfc
Rui Feng <394@holbertonschool.com>
2018-11-13 18:33:47 +0000

    import after docstring

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
 """ init models """
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
