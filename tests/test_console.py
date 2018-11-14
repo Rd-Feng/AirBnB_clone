6eeab373d69a47ac788cc9b67663f3493c8e1bf1
Stephen Chu <stephen.chu530@gmail.com>
2018-11-13 20:21:08 -0800

    feat: initial commit

---
+++ tests/test_console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Test Console Module'''
+
+import unittest
+import pep8
+import sys
+import io
+import sys
+import models
+import json
+from os import remove
+from os.path import isfile
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class Test_01_Basic:
+    '''Test Console Basic'''
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
